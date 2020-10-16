e8474bdd544af06d62ebdd74e131c98e50371cfe
RepoMan <monsterknack@protonmail.com>
2020-10-16 21:02:55 +0200

    Lyckades koppla databas

---
+++ src/main/java/projektarbete/demo/House.java
@@ -0,0 +1,82 @@
+package projektarbete.demo;
+
+public class House {
+    private String country;
+    private String city;
+    private String size;
+    private Integer noOfBeds;
+    private String amenities;
+    private String picture;
+    private String description;
+    private int id;
+
+
+
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Integer getNoOfBeds() {
+        return noOfBeds;
+    }
+
+    public void setNoOfBeds(Integer noOfBeds) {
+        this.noOfBeds = noOfBeds;
+    }
+
+    public String getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(String amenities) {
+        this.amenities = amenities;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
+
---
+++ src/main/java/projektarbete/demo/repository/IHouseCrud.java
@@ -0,0 +1,9 @@
+package projektarbete.demo.repository;
+import projektarbete.demo.House;
+
+import java.util.List;
+
+public interface IHouseCrud {
+    public List<House> getAllHouses();
+
+}// end interface
