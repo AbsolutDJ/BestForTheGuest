2d4360444211688f4636aed1490e53146c25dc1c
RepoMan <monsterknack@protonmail.com>
2020-10-20 13:39:26 +0200

    Fixade lite funktionalitet på hemsidan

--- src/main/java/projektarbete/demo/controllers/FormController.java
+++ src/main/java/projektarbete/demo/controllers/FormController.java
@@ -3,39 +3,30 @@ package projektarbete.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import projektarbete.demo.service.HouseService;
 
 @Controller
-public class HouseController {
+public class FormController {
 
     @Autowired //Annotation som talar om för container att göra en dependency injection, den letar reda på HouseService.
-    private HouseService houseService;
+   private HouseService houseService;
 
 
-
-
-    @RequestMapping("/home")
-    public String getHouses(Model model){
-        model.addAttribute("houses" , houseService.getAllHouses());
-        return "index";
-
-    }
-
-
-    @RequestMapping("/house/{id}")
-    public String getHouse(Model model, @PathVariable(required = false) int id){
+    @RequestMapping("/house/edit/{id}")
+    public String editHouse(Model model, @PathVariable(required = false) int id){
         for (int i = 0; i < houseService.getAllHouses().size(); i++){
             if(id == houseService.getAllHouses().get(i).getId()){
-                model.addAttribute("house",houseService.getAllHouses().get(i));
+                model.addAttribute("houseform",houseService.getAllHouses().get(i));
             }
 
         }
-        return "privatehouseview";
+        return "formview";
+
+    }
 
 }
 
-}
+
